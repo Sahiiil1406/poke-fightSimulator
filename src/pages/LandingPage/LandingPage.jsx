6f6e7dd793cd7028f03079b39851b3d70f7dd70b
alphaplayerofdooms <154459496+alphaplayerofdooms@users.noreply.github.com>
2024-09-25 02:46:35 +0530

    Teams Final Changes

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
 import InfoSection from "./components/InfoSection";
-import TeamCarousel from "./components/TeamCarousel";
+import TeamCarousel from "./components//Teams/TeamCarousel";
 
 export default function LandingPage() {
 	return (
