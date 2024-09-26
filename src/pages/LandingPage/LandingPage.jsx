3e39b11975424a0f201ae70e2b020dfcae1ba1d7
sogalabhi <abhijithsogal@gmail.com>
2024-09-26 07:03:35 +0530

    layout done

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import HeroSection from "./components/HeroSection";
 import InfoSection from "./components/InfoSection";
 import TeamCarousel from "./components/TeamCarousel";
+import OurVision from "./components/OurVision";
 
 export default function LandingPage() {
 	return (
 		<>
 			<HeroSection />
 			<InfoSection />
+			<OurVision/>
 			<TeamCarousel />
 		</>
 	);
