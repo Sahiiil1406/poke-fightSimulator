f0a5169e3b1f1ddc617b0ce82f49e9d74a6c91a0
alphaplayerofdooms <154459496+alphaplayerofdooms@users.noreply.github.com>
2024-09-27 23:21:21 +0530

    waterfall image in gallary section

--- src/pages/GalleryPage/GalleryPage.jsx
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import PhotoGrid from "./PhotoGrid";
+import Carousel from "./Carousel";
 
 export default function GalleryPage() {
-	return <div>GalleryPage</div>;
+	return <div className="flex flex-col mt-[10%]">
+		<Carousel/>
+		<PhotoGrid />
+		</div>;
 }
